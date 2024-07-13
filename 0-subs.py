4a6cfe22aacd9f63690e544bc23ecea4a66b8dd3
wenebifid <o.fiderikum1@alustudent.com>
2024-07-13 16:47:39 +0000

    Done

---
+++ 0-subs.py
@@ -0,0 +1,19 @@
+
+n that queries the 'Reddit API' and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}  # avoid Too Many Requests error
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()["data"]
+        return data["subscribers"]
+    else:
+        return 0:
